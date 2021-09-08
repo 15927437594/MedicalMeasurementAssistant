77ec5ddc2ee8f0798e69cf3e38199a6991ea4a9c
duji <15927437594@163.com>
2021-09-08 08:02:16 +0800

    [add]:完成设备数据解析功能

--- app/src/main/java/cn/com/medicalmeasurementassistant/protocol/BaseReceiveProtocol.java
+++ app/src/main/java/cn/com/medicalmeasurementassistant/protocol/BaseReceiveProtocol.java
@@ -35,18 +35,29 @@ public abstract class BaseReceiveProtocol extends Protocol {
     public boolean unpack(List<Integer> srcData) {
         byte[] bytes = CalculateUtils.integerListToBytes(srcData);
         LogUtils.i(CalculateUtils.bytesToHex(bytes));
+        List<Integer> crcData;
         boolean result;
         version = srcData.get(1);
         source = srcData.get(2);
         destination = srcData.get(3);
         action = CalculateUtils.highLowToInt(srcData.get(4), srcData.get(5));
-        length = CalculateUtils.highLowToInt(srcData.get(6), srcData.get(7));
-        data.addAll(srcData.subList(8, length + 8));
-        crc1 = srcData.get(srcData.size() - 2);
-        crc2 = srcData.get(srcData.size() - 1);
-        List<Integer> crcData = new ArrayList<>(srcData.subList(0, length + 8));
+        if (action == 0x0105) {
+            crcData = new ArrayList<>(srcData.subList(0, 6));
+        } else {
+            length = CalculateUtils.highLowToInt(srcData.get(6), srcData.get(7));
+            data.addAll(srcData.subList(8, length + 8));
+            crcData = new ArrayList<>(srcData.subList(0, length + 8));
+        }
+        crc2 = srcData.get(srcData.size() - 2);
+        crc1 = srcData.get(srcData.size() - 1);
         byte[] crcDataBytes = CalculateUtils.integerListToBytes(crcData);
-        result = CalculateUtils.checkCrc(crcDataBytes, crc1, crc2);
+        if (action == 0x0403) {
+            // action=0x0403的数据不用做crc校验
+            result = true;
+        } else {
+            result = CalculateUtils.checkCrc(crcDataBytes, crc1, crc2);
+        }
+
         if (!result) {
             length = 0x0000;
             data.clear();
