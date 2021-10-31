e2c24150342149398653a3671694a14665b060f1
jid <jid@hwtc.com.cn>
2021-11-01 01:14:56 +0800

    [pref]:优化代码

--- app/src/main/java/cn/com/medicalmeasurementassistant/utils/MeasurementFileUtils.java
+++ app/src/main/java/cn/com/medicalmeasurementassistant/utils/MeasurementFileUtils.java
@@ -1,13 +1,8 @@
 package cn.com.medicalmeasurementassistant.utils;
 
-import android.content.Context;
-import android.net.Uri;
-import android.os.ParcelFileDescriptor;
-
 import com.blankj.utilcode.util.FileIOUtils;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
@@ -29,44 +24,18 @@ public class MeasurementFileUtils {
         String saveTime = "Saving DateTime: " + CalculateUtils.getTime() + "\n";
         String sampleFrequency = "Sampling frequency: 2000" + "\n";
         String channels = "Number of channels: " + Constant.DEFAULT_CHANNEL + "\n";
-        String dataNumber = "Number of data: " + originalData.size() + "\n";
+        String originDataNumber = "Number of data: " + originalData.size() + "\n";
+        String filterDataNumber = "Number of data: " + originalData.size() + "\n";
         String unit = "Unit: mV" + "\n";
         String title = "CH1,CH2,CH3,CH4,CH5,CH6,CH7,CH8,Capacitor/pF\n";
         String originContent = originalData.toString().replace("[", "").replace("]", "").replace(" ", "")
                 .replace("\n,", "\n");
         String filterContent = filterData.toString().replace("[", "").replace("]", "").replace(" ", "")
                 .replace("\n,", "\n");
-        String originContentWrite = saveTime + sampleFrequency + channels + dataNumber + unit + title + originContent;
-        String filterContentWrite = saveTime + sampleFrequency + channels + dataNumber + unit + title + filterContent;
-        saveMeasurementFileForFile(originalFile, originContentWrite);
-        saveMeasurementFileForFile(filterFile, filterContentWrite);
-    }
-
-    public synchronized static void saveMeasurementFileForFile(File file, String content) {
-        FileIOUtils.writeFileFromString(file, content);
-    }
-
-    public void saveToCSVFile(Context context, Uri uri) {
-        StringBuilder d = new StringBuilder();
-        d.append("col1,col2,col3\n");
-        d.append("1,2,3\n");
-        d.append("2,3,4\n");
-        String data = d.toString();
-
-// Get a file output stream however is compatible with your app. Showing one of
-// many ways here.
-// once you have it, call outputStream.write(data.getBytes());
-        try {
-            ParcelFileDescriptor pfd = context.getContentResolver().openFileDescriptor(uri, "w");
-            if (pfd != null) {
-                FileOutputStream fileOutputStream = new FileOutputStream(pfd.getFileDescriptor());
-                fileOutputStream.write(data.getBytes());
-                fileOutputStream.close();
-                pfd.close();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        String originContentWrite = saveTime + sampleFrequency + channels + originDataNumber + unit + title + originContent;
+        String filterContentWrite = saveTime + sampleFrequency + channels + filterDataNumber + unit + title + filterContent;
+        FileIOUtils.writeFileFromString(originalFile, originContentWrite);
+        FileIOUtils.writeFileFromString(filterFile, filterContentWrite);
     }
 
     /**
