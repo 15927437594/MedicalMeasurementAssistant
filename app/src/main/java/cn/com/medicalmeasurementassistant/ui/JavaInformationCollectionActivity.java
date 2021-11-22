a0b8b7e0fe28557c3fcec23f170a54ddbcc5e2c8
shixuhui <shixuhui1993@163.com>
2021-11-22 16:28:46 +0800

    1

--- app/src/main/java/cn/com/medicalmeasurementassistant/ui/JavaInformationCollectionActivity.java
+++ app/src/main/java/cn/com/medicalmeasurementassistant/ui/JavaInformationCollectionActivity.java
@@ -14,6 +14,7 @@ import androidx.core.content.ContextCompat;
 
 import java.util.List;
 
+
 import cn.com.medicalmeasurementassistant.R;
 import cn.com.medicalmeasurementassistant.base.BaseKotlinActivity;
 import cn.com.medicalmeasurementassistant.entity.Constant;
